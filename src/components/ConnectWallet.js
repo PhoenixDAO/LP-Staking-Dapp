1975ef678dc2db277715bc7013a9a73238078762
hakeemullahjan <hakeemullah.yousufzai@gmail.com>
2021-09-27 19:01:54 +0500

    coinbase wallet

--- src/components/ConnectWallet.js
+++ src/components/ConnectWallet.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useState, useEffect } from "react";
 import {
   Box,
   Button,
@@ -16,9 +16,12 @@ import {
   UserRejectedRequestError,
 } from "@web3-react/injected-connector";
 import { WalletConnectConnector } from "@web3-react/walletconnect-connector";
+import { WalletLinkConnector } from "@web3-react/walletlink-connector";
+
 import { injected } from "../utils/web3Connectors";
-import { walletconnect } from "../utils/web3ConnectFunctions";
+import { walletconnect, walletlink } from "../utils/web3ConnectFunctions";
 import { conciseAddress } from "../utils/formatters";
+import { formatEther } from "@ethersproject/units";
 
 import CloseIcon from "@mui/icons-material/Close";
 import Logo from "../assets/Logo.png";
@@ -64,13 +67,38 @@ const Item = styled("button")(({ theme }) => ({
 export default function ConnectWallet() {
   const web3context = useWeb3React();
 
-  const { account, active, connector, deactivate } = web3context;
-
-  console.log(account);
+  const { account, active, connector, deactivate, library, chainId } =
+    web3context;
 
   const [open, setOpen] = useState(false);
+  const [balance, setBalance] = useState(0);
+
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
+
+  useEffect(() => {
+    if (!!account && !!library) {
+      let stale = false;
+
+      library
+        .getBalance(account)
+        .then((balance) => {
+          if (!stale) {
+            setBalance(balance);
+          }
+        })
+        .catch(() => {
+          if (!stale) {
+            setBalance(null);
+          }
+        });
+
+      return () => {
+        stale = true;
+        setBalance(undefined);
+      };
+    }
+  }, [account, library, chainId]);
 
   const activateWallet = useCallback(
     (connector, onClose = () => {}) => {
@@ -112,6 +140,9 @@ export default function ConnectWallet() {
     if (connector instanceof WalletConnectConnector) {
       await connector.close();
     }
+    if (connector instanceof WalletLinkConnector) {
+      await connector.close();
+    }
 
     // onSuccess();
   };
@@ -132,6 +163,9 @@ export default function ConnectWallet() {
 
   return (
     <div>
+      <span>
+        {balance === null ? "Error" : balance ? `Ξ${formatEther(balance)}` : ""}
+      </span>
       <Button onClick={handleOpen} variant="outlined">
         {active && account ? conciseAddress(account) : "  Connect Wallet"}
       </Button>
@@ -233,29 +267,38 @@ export default function ConnectWallet() {
               )}
             </Item>
             <Divider />
-            <Item>
+            <Item
+              onClick={() => {
+                !active && !(connector instanceof WalletLinkConnector)
+                  ? activateWallet(walletlink)
+                  : deactivateWallet();
+              }}
+            >
               <img src={coinbaseIcon} alt="logo" />
+
               <Typography
                 id="modal-modal-title"
                 variant="h6"
                 component="h2"
                 sx={{ ml: 3 }}
               >
-                Coinbase Wallet
+                {active && connector instanceof WalletLinkConnector
+                  ? "Disconnect  Coinbase Wallet"
+                  : "Coinbase Wallet"}
               </Typography>
-              <Typography
-                variant="body2"
-                sx={{
-                  ml: "auto",
-                  color: "#ED2020",
-                  background: "#FAEBEB",
-                  px: "7px",
-                  py: "3px",
-                  borderRadius: "30px",
-                }}
-              >
-                COMING SOON
-              </Typography>
+              {active && connector instanceof WalletLinkConnector ? (
+                <CheckCircleOutlineIcon
+                  sx={{ ml: "auto" }}
+                  fontSize="large"
+                  color="primary"
+                />
+              ) : (
+                <ArrowRightAltIcon
+                  sx={{ ml: "auto" }}
+                  fontSize="large"
+                  color="primary"
+                />
+              )}
             </Item>
             <Divider />
             <Item>
