9011edaa5651e09325115389cfb2f885f635cb28
hakeemullahjan <hakeemullah.yousufzai@gmail.com>
2021-10-11 14:35:39 +0500

    css styles changes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import "./App.css";
 import App from "./App";
+import "./App.css";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
